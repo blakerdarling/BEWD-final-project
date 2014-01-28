6649f7db4e99fcaacab36acb74a37dbc82d5a939
blakerdarling <blakerdarling@gmail.com>
2014-01-28 18:13:54 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LureLog::Application.config.secret_key_base = '01a635b92077942fe10afc6e2786b5d3d4e6d4ec90271ea6f9f58af6eb6b01ac3a9138ed96eb023f6fec12209fd6f6bce0019124ed9b6040ab99141a79f5ecd2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LureLog::Application.config.session_store :cookie_store, key: '_lure_log_session'
